60c47838baac7f8ac5474e7cda15b128e231d243
Sean McNamara <smcnam@gmail.com>
2016-12-13 15:35:02 -0500

    Add Apache License v2.0 headers.

--- convert.cpp
+++ convert.cpp
@@ -1,3 +1,20 @@
+/*
+This file is part of kynnaugh-cc.
+
+Kynnaugh-cc is free software: you can redistribute it and/or modify
+it under the terms of the Apache License, version 2.0 as published by
+the Apache Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Apache License, version 2.0 for more details.
+
+You should have received a copy of the Apache License, version 2.0
+along with kynnaugh-cc.  If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
 #include "convert.h"
 
 #include <QGst/ElementFactory>
